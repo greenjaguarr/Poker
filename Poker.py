e34e2d3098c1ab0f4794764bb542d6c9b3e37959
greenjaguarr <127725326+greenjaguarr@users.noreply.github.com>
2023-11-03 11:36:06 +0100

    Update Poker.py
    extra method bij class kaart toegevoegd

--- Poker.py
+++ Poker.py
@@ -5,6 +5,8 @@ class Kaart:
 
     def show(self):
         print(f"{self.kleur} {self.naam}")
+    def get(self):
+        return (self.kleur, self.naam)
 
 namen = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "B", "V", "H", "A")
 kleuren = ("Harten", "Klaveren", "Ruiten", "Schoppen")
