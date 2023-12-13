506d455bbecc1310f09dd43a8d3aa55d82f58252
greenjaguarr <127725326+greenjaguarr@users.noreply.github.com>
2023-12-13 20:00:27 +0000

    I added a if __name__ == __main__ to the kaartspel module to make it not broken.

--- kaartspel.py
+++ kaartspel.py
@@ -7,11 +7,12 @@ class Kaart:
         print(f"{self.kleur} {self.naam}")
 
 
-namen = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "B", "V", "H", "A")
-kleuren = ("Harten", "Klaveren", "Ruiten", "Schoppen")
-kaarten = []
-for naam in namen:
-    for kleur in kleuren:
-        kaarten.append(Kaart(kleur, naam))
-# for kaart in kaarten:
-#     kaart.show()
+if __name__ == '__main__':
+    namen = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "B", "V", "H", "A")
+    kleuren = ("Harten", "Klaveren", "Ruiten", "Schoppen")
+    kaarten = []
+    for naam in namen:
+        for kleur in kleuren:
+            kaarten.append(Kaart(kleur, naam))
+            # for kaart in kaarten:
+            #     kaart.show()
